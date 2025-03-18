eef73c8afe666f638a608b28a6ca3815a2db17b8
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-03-18 08:44:34 -0300

    Tentando resolver bug

--- script/revelarSenha.js
+++ script/revelarSenha.js
@@ -1,16 +1,17 @@
 const senhaInput = document.getElementById("senha");
+const repitirSenha = document.getElementById("senha2");
 const toggleSenha = document.getElementById("toggleSenha");
-document.addEventListener("DOMContentLoaded", function () {
-    
 
+document.addEventListener("DOMContentLoaded", function () {
     toggleSenha.addEventListener("click", () => {
         if (senhaInput.type === "password") {
             senhaInput.type = "text";
+            repitirSenha.type = "text";
             toggleSenha.src = "/img/lock-open-alt-regular-24.png";
         } else {
             senhaInput.type = "password";
-            toggleSenha.src = "/img/lock-alt-solid-24.png"; 
+            repitirSenha.type = "password";
+            toggleSenha.src = "/img/lock-alt-solid-24.png";
         }
     });
-
 });
